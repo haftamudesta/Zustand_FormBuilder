9a0206a944a1cab8fb5f62bbfb60fbcc460a5e6e
haftamudesta <desta451616@hotmail.com>
2024-10-29 01:45:33 -0800

    Add zustand store

---
+++ src/store/Store.ts
@@ -0,0 +1,25 @@
+import { create } from 'zustand'
+
+interface formFields{
+        label:string,
+        type:'text'|'number'|'password'|'textarea'|'date'|'file';
+        value:string,
+}
+interface formStoreState{
+        formFields:formFields[],
+        addField:(field:formFields)=>void,
+        removeField:(index:number)=>void,
+        updateField:(index:number,updatedField:formFields)=>void,
+        resetForm:()=>void,
+}
+
+export const useFormFields=create<formStoreState>((set)=>({
+        formFields:[],
+        addField: (field)=>set((state)=>({formFields:[...state.formFields,field]})),
+        removeField:(index)=>set((state)=>({formFields:state.formFields.filter((_,i)=>i!==index)})),
+        
+        updateField:(index,updatedField)=>set((state)=>({
+                formFields: state.formFields.map((field,i)=>i===index?updatedField:field)})),
+
+        resetForm:()=>set({formFields:[]})
+}))
